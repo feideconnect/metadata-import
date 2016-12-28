6078ea7f334f62871eff0580e503bf53396ca392
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-12-28 11:13:58 +0100

    Update build number

--- bin/_config.sh
+++ bin/_config.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 export KUBERNETES_DEPLOYMENT="getmetadata"
-export VERSION="1.1.2"
+export VERSION="1.1.4"
 export IMAGE="eu.gcr.io/turnkey-cocoa-720/$KUBERNETES_DEPLOYMENT:$VERSION"
